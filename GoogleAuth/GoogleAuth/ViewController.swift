e471dd1684cdbe92f0a4ab55e6d3bb8e989a283f
Usman Turkaev <u.turkaev@yandex.ru>
2021-09-23 23:52:35 +0300

    Create Xcode project

---
+++ GoogleAuth/GoogleAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoogleAuth
+//
+//  Created by Усман Туркаев on 23.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
