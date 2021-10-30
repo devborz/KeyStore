b6a93c5df9cc009d6ea90a0e4513edac0ce2075a
Usman Turkaev <u.turkaev@yandex.ru>
2021-10-30 04:32:28 +0300

    Create DBManager.swift

---
+++ KeyStore/KeyStore/DBManager.swift
@@ -0,0 +1,91 @@
+//
+//  AccountsManager.swift
+//  KeyStore
+//
+//  Created by Усман Туркаев on 24.09.2021.
+//
+
+import Foundation
+import RealmSwift
+import FirebaseFirestore
+
+class DBManager {
+    
+    static var shared = DBManager()
+    
+    var accounts = DefinedObservable<[AccountCellViewModel]>([])
+    
+    private var db = Firestore.firestore()
+    
+    var isFirstLoadMade = false
+    
+    private init() {
+    }
+    
+    func prepareData() {
+        clear()
+        getSavedAccounts()
+    }
+    
+    func clear() {
+        isFirstLoadMade = false
+        accounts.value = []
+    }
+    
+    func createUser(id: String, email: String, completion: ((Error?) -> Void)?) {
+        let batch = db.batch()
+        let userRef = db.collection("users").document(id)
+        batch.setData(["id": id, "email": email], forDocument: userRef)
+        batch.commit { error in
+            completion?(error)
+        }
+    }
+    
+    func getSavedAccounts() {
+        guard let uid = AuthManager.shared.currentUserID else { return }
+        db.collection("users").document(uid).collection("accounts").getDocuments { snapshot, error in
+            guard error == nil,
+                  let docs = snapshot?.documents else { return }
+            var accounts: [Account] = []
+            for doc in docs {
+                if let account = Account(dict: doc.data()) {
+                    accounts.append(account)
+                }
+            }
+            self.isFirstLoadMade = true
+            self.accounts.value = accounts.map { AccountCellViewModel(model: $0) }
+        }
+    }
+    
+    func fetchLastCodeForAccount(_ account: Account) -> Code? {
+        if let realm = try? Realm() {
+            return realm.object(ofType: Code.self, forPrimaryKey: account.id)
+        } else {
+            return nil
+        }
+    }
+    
+    func saveAccount(_ account: Account) {
+        accounts.value.append(.init(model: account))
+        guard let uid = AuthManager.shared.currentUserID else { return }
+        db.collection("users").document(uid).collection("accounts").document(account.id).setData(account.dict)
+    }
+    
+    func deleteAccount(_ index: Int) {
+        let account = accounts.value[index].model
+        accounts.value.remove(at: index)
+        guard let uid = AuthManager.shared.currentUserID else { return }
+        db.collection("users").document(uid).collection("accounts").document(account.id).delete()
+    }
+    
+    func saveCode(_ code: Code, for account: Account) {
+        if let realm = try? Realm() {
+            try? realm.write {
+                if let lastCode = realm.object(ofType: Code.self, forPrimaryKey: account.id) {
+                    realm.delete(lastCode)
+                }
+                realm.add(code)
+            }
+        }
+    }
+}
