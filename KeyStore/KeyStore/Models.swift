43dcad6c0fbd276deb0a907fe9e299038804fb26
Usman Turkaev <u.turkaev@yandex.ru>
2021-10-30 04:33:30 +0300

    Update Models.swift

--- KeyStore/KeyStore/Models.swift
+++ KeyStore/KeyStore/Models.swift
@@ -9,18 +9,57 @@ import Foundation
 import CryptoKit
 import RealmSwift
 
-class Account: Object {
-    @Persisted var id: String = ""
+struct Account {
+    var id: String = ""
     
-    @Persisted var issuer: String = ""
+    var issuer: String = ""
     
-    @Persisted var email: String = ""
+    var email: String = ""
     
-    @Persisted var secret: String = ""
+    var secret: String = ""
+    
+    var dict: [String: Any] {
+        return [
+            "id": id,
+            "issuer": issuer,
+            "email": email,
+            "secret": secret
+        ]
+    }
+}
+
+extension Account {
+    init?(dict: [String: Any]) {
+        guard let id = dict["id"] as? String,
+              let issuer = dict["issuer"] as? String,
+              let email = dict["email"] as? String,
+              let secret = dict["secret"] as? String else { return nil }
+        self.init(id: id, issuer: issuer, email: email, secret: secret)
+    }
 }
 
 class Code: Object {
     @Persisted(primaryKey: true) var _id: String
     @Persisted var code: String = ""
     @Persisted var created: Date = Date()
+}
+
+struct DBUser {
+    let id: String
+    let email: String
+    
+    var dict: [String: Any] {
+        return [
+            "id": id,
+            "email": email
+        ]
+    }
+}
+
+extension DBUser {
+    init?(dict: [String: Any]) {
+        guard let id = dict["id"] as? String,
+              let email = dict["email"] as? String else { return nil }
+        self.init(id: id, email: email)
+    }
 }
