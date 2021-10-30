425ad94398a98015c9c211d3959751628726c14c
Usman Turkaev <u.turkaev@yandex.ru>
2021-10-30 04:32:30 +0300

    Create Extensions.swift

---
+++ KeyStore/KeyStore/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  KeyStore
+//
+//  Created by Усман Туркаев on 30.10.2021.
+//
+
+import UIKit
+
+extension String {
+    var image: UIImage? {
+        return UIImage(systemName: self)
+    }
+}
