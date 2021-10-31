57ccf3abd3792a0e62710ab47c1bfb1c067fbc3d
Usman Turkaev <u.turkaev@yandex.ru>
2021-10-31 17:58:51 +0300

    Update Extensions.swift

--- KeyStore/KeyStore/Extensions.swift
+++ KeyStore/KeyStore/Extensions.swift
@@ -12,3 +12,20 @@ extension String {
         return UIImage(systemName: self)
     }
 }
+
+extension Date {
+    static var nearestPointDivisibleBy30: Date {
+        let currentDate = Date()
+        let components = Calendar.current.dateComponents([.hour, .minute, .second], from: currentDate)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        let second = components.second ?? 0
+        if second < 29 {
+            let date = Calendar.current.date(bySettingHour: hour, minute: minute, second: 0, of: currentDate)
+            return date ?? currentDate
+        } else {
+            let date = Calendar.current.date(bySettingHour: hour, minute: minute, second: 30, of: currentDate)
+            return date ?? currentDate
+        }
+    }
+}
